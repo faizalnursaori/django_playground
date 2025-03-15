6327d14adfcebcb5c366d29574fc4fec70c8084e
Faizal Nursaori <faizalrahmatn@gmail.com>
2025-03-16 01:45:12 +0700

    feat: add dashboard notes, and showing the data

--- notes/urls.py
+++ notes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard_home_view, dashboard_orders_view, dashboard_profile_view
+from .views import dashboard_home_view, dashboard_orders_view, dashboard_profile_view,dashboard_notes_view
 
 urlpatterns = [
     path('', dashboard_home_view, name="dashboard_home"),
     path('orders/', dashboard_orders_view, name='dashboard_orders'),
+    path('notes/', dashboard_notes_view, name='dashboard_notes'),
     path('profile/', dashboard_profile_view, name='dashboard_profile')
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Notes
 
 # Create your views here.
 def dashboard_home_view(request):
@@ -6,6 +7,14 @@ def dashboard_home_view(request):
 
 def dashboard_orders_view(request):
     return render(request, 'dashboard/dashboard_orders.html')
+
+def dashboard_notes_view(request):
+    notes = Notes.objects.all()
+    context = {
+        "notes": notes
+    }
+
+    return render(request, 'dashboard/dashboard_notes.html', context)
 
 def dashboard_profile_view(request):
     return render(request, 'dashboard/dashboard_profile.html')
